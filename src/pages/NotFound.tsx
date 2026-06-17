e798f27a5b97314585fc4b36a26248efc01e71b2
vlog-816 <vyvy.tester@gmail.com>
2026-06-17 20:09:28 +0700

    align page 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center bg-state-50">
+    //<div className="flex items-center justify-center min-h-screen text-center bg-state-50">
+    <div className="relative flex items-center justify-center h-screen w-full overflow-hidden">
       <Link to={"/login"}>
         <img
           src="404-not-found-1.png"
           alt="not found"
-          className="max-w-full mb-6"
+          className="absolute inset-0 w-full h-full object-cover object-center"
         />
-        <span className="mt-4 inline-flex items-center gap-2 px-4 py-2 bg-indigo-600 text-white text-sm font-medium rounded-lg hover:bg-indigo-700 transition-colors">
-          Go home
+        <span className="relative inline-flex items-center px-4 py-2 bg-indigo-600 text-white text-lg font-medium rounded-lg hover:bg-indigo-700 transition-colors">
+             Go home
         </span>
       </Link>
     </div>
